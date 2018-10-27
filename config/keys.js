d558c260298d0a6d70cab34132e81cd481d69b27
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-10-27 18:58:27 +0530

    Google strategy completed

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb://kishan:kishan1219@ds133582.mlab.com:33582/storybooks-dev",
+  googleClientID:
+    "949188436745-gp770no05q9fchnvsu1v17vugeh1g9q6.apps.googleusercontent.com",
+  googleClientSecret: "06cNMVoUJM0Ic5_ljsAMPTOM"
+};
