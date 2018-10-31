72c5e3dd3c85d00fa2be034e517f10f790047a49
patelsaheb1219 <patelsaheb1219@gmail.com>
2018-10-31 10:33:17 +0530

    Update keys files

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://kishan:kishan1219@ds133582.mlab.com:33582/storybooks-dev",
-  googleClientID:
-    "949188436745-gp770no05q9fchnvsu1v17vugeh1g9q6.apps.googleusercontent.com",
-  googleClientSecret: "06cNMVoUJM0Ic5_ljsAMPTOM"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("/keys_prod");
+} else {
+  module.exports = require("/keys_dev");
+}
